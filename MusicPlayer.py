885cacfb57bf61ebae562b5ca5c141f849ed8947
ReeseRuffin <Reese.Ruffin@Genesys.com>
2023-01-10 18:41:32 -0500

    first GUI project

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -8,6 +8,63 @@ from tkinter import filedialog
 musicPlayer = Tk()
 musicPlayer.title("Reese Music Player")
 
+#--------------------------------------------------------COMMANDS-------------------------------------------------------
+#Volume control
+volumeControl = float(0.5) 
+
+#This will play the track file that is stored on the device
+def playTrack():
+    fileName  = filedialog.askopenfile(initialdir="E:/", title="Please Select File")
+    currentTrack = fileName
+#==========FIX IN THE FUTURE====================
+    # trackName = fileName.split("/")
+    # trackName = trackName[-1]
+#==========FIX IN THE FUTURE====================
+    print(fileName)
+
+    try:
+        mixer.init()
+        mixer.music.load(fileName)
+        mixer.music.set_volume(volumeControl)
+        mixer.music.play()
+        Track_Label.config(fg="blue",text="Now Playing : " + str(fileName) )
+        volumeLable.config(fg="black",text="Volume: " + str(volumeControl))
+    except Exception as e:
+        print(e)
+        Track_Label.config(fg="black", text="Error playing track")
+
+def reduceSound():
+    try:
+        global volumeControl
+        if volumeControl <= 0:
+            volumeLable.config(fg="red", text="Volume : Muterd")
+            return
+        volumeControl = volumeControl - float(0.1)
+        volumeControl = round(volumeControl, 1)
+        mixer.music.set_volume(volumeControl)
+        volumeLable.config(fg="green", text="Volume : " + str(volumeControl))
+    except Exception as e:
+        print(e)
+        Track_Label.config(fg="Red", text="Track isn't selected")
+
+def increaseVolume():
+    try:
+        global volumeControl
+        if volumeControl >= 1:
+            volumeLable.config(fg="Green", text="Volume : Max")
+            return
+        volumeControl = volumeControl + float(0.1)
+        volumeControl = round(volumeControl, 1)
+        mixer.music.set_volume(volumeControl)
+        volumeLable.config(fg="green", text="Volume : " + str(volumeControl))
+    except Exception as e:
+        print(e)
+        Track_Label.config(fg="Red", text="Track isn't selected")
+
+
+
+
+
 #--------------------------------------------------------LABLE-------------------------------------------------------
 
 #Labels
@@ -24,12 +81,13 @@ volumeLable = Label(musicPlayer,font=("Calibri",13))
 volumeLable.grid(sticky="N",row=5)
 
 #Buttons
-Btn(musicPlayer, text="Select Track", font=("Calibri", 12)).grid(row=2,sticky="N")
+Btn(musicPlayer, text="Select Track", font=("Calibri", 12),command=playTrack).grid(row=2,sticky="N")
 Btn(musicPlayer, text="Pause", font=("Calibri",12)).grid(row=3,sticky="E")
 Btn(musicPlayer, text="Resume", font=("Calibri",12)).grid(row=3,sticky="W")
-Btn(musicPlayer, text="-", font=("Calibri",12),width=5).grid(row=5,sticky="W")
-Btn(musicPlayer, text="+", font=("Calibri",12),width=5).grid(row=5,sticky="E")
+Btn(musicPlayer, text="-", font=("Calibri",12),width=5, command=reduceSound).grid(row=5,sticky="W")
+Btn(musicPlayer, text="+", font=("Calibri",12),width=5, command=increaseVolume).grid(row=5,sticky="E")
 
-#--------------------------------------------------------LABLE-------------------------------------------------------
+
+
 
 musicPlayer.mainloop()
